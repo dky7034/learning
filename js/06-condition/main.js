029164c0d7726beeb3c6ae11d46340a79354e8d1
artp <dky7034@naver.com>
2025-09-18 13:00:25 +0900

    condition

---
+++ js/06-condition/main.js
@@ -0,0 +1,22 @@
+// 문제 1
+let number = -1;
+
+if (number > 0) {
+  console.log("양수");
+} else if (number < 0) {
+  console.log("음수");
+} else {
+  console.log("0");
+}
+
+console.log("--------------------------------");
+
+// 문제 2
+let number2 = 0;
+if (number > 0) {
+  console.log("양수");
+} else if (number2 < 0) {
+  console.log("음수");
+} else {
+  console.log("0");
+}
